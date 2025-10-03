e3bfa64ef513b787fd9b03f22fa686013f70d91a
Vanessa-Maria2 <vanessa.silva5205@gmail.com>
2025-10-03 14:47:38 -0300

    first commit

---
+++ src/test/java/com/example/ourlibrary/OurlibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ourlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurlibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
